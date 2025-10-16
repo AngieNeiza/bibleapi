0904647bd2e87d53fa8fde68ac9048579506549b
AngieNeiza <angie.neizac@uniagustiniana.edu.co>
2025-10-16 14:06:30 +0000

    actualizacion

--- js/informativa.js
+++ js/informativa.js
@@ -1,8 +1,8 @@
 function Informativa() {
     document.getElementById("root").innerHTML = `<section class="info">
         <h1><a href="https://bible-api.com/">BIBLE API</a></h1>
-        <h2>Tu nombre</h2>
-        <img src="https://cdn-icons-png.flaticon.com/512/3771/3771429.png" alt="Bible Icon">
+        <h2>angie neiza</h2>
+        <img src="https://static.wixstatic.com/media/b78a8a_dfd382dea45f4b9f851cb1af65dd817f~mv2.png/v1/fill/w_1000,h_563,al_c,q_90,usm_0.66_1.00_0.01/b78a8a_dfd382dea45f4b9f851cb1af65dd817f~mv2.png" alt="Bible Icon">
         <p>API con información de la Biblia completa</p>
         <p>Incluye todos los libros del Antiguo y Nuevo Testamento</p>
         <p>Total de capítulos disponibles: ${totalCapitulos}</p>
